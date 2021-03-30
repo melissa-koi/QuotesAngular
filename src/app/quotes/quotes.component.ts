b62cdf2ec4042793e19aafe95ca69672706c7fab
melissa-koi <melissawangui3@gmail.com>
2021-03-30 06:30:07 +0300

    final touches

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,9 +9,9 @@ import { Quotes } from '../quotes';
 export class QuotesComponent implements OnInit {
 
   quote:Quotes[] = [
-    new Quotes('I am Groot', 'Guardians of the Galaxy' ,'Anonymous',new Date(2021,3,14)),
-    new Quotes("If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.", 'James Cameron' ,'Sandy',new Date(2021,4,14)),
-    new Quotes("You miss 100% of the shots you don't take", 'Michael Scott' ,'Bob',new Date(2021,4,14)),
+    new Quotes('I am Groot', 'Guardians of the Galaxy' ,'Anonymous',new Date(2021,2,26)),
+    new Quotes("If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.", 'James Cameron' ,'Sandy',new Date(2021,2,27)),
+    new Quotes("You miss 100% of the shots you don't take", 'Michael Scott' ,'Bob',new Date(2021,2,28)),
   ];
 
   toggleDetails(index) {
